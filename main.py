132fe0d41d3d53832033b76b560f31419b3e30d1
Jiaman Betty Wu <46700309+JiamanBettyWu@users.noreply.github.com>
2020-10-14 23:30:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+    return 'Hello World! +1'
 
 @app.route('/newroute/<name>')
 def newroute(name):
